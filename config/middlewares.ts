956ce675c4a755f039cec5d2fa5eb4b17561d6d4
Hasibur Rahman <hasib.webdev@gmail.com>
2023-04-25 17:32:24 +0600

    s3 done

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,6 +1,6 @@
-export default [
+export default ({ env }) => [
   "strapi::errors",
-  "strapi::security",
+  // "strapi::security",
   "strapi::cors",
   "strapi::poweredBy",
   "strapi::logger",
@@ -11,13 +11,25 @@ export default [
   "strapi::public",
 
   {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        directives: {
+          'script-src': ["'self'", "'unsafe-inline'", 'cdn.jsdelivr.net'],
+          'img-src': ["'self'", 'data:', 'blob:', 'image.tmdb.org', 'mlwbd.skin', 'cdn.jsdelivr.net', 'strapi.io', `s3.${env('AWS_REGION')}.amazonaws.com`, '*'],
+        },
+      }
+    },
+  },
+
+  {
     name: "strapi::body",
     config: {
       formLimit: "256mb", // modify form body
       jsonLimit: "256mb", // modify JSON body
       textLimit: "256mb", // modify text body
       formidable: {
-        maxFileSize: 3000000000, // multipart data, modify here limit of uploaded file size
+        maxFileSize: 3000000000, // 3GB multipart data, modify here limit of uploaded file size
       },
     },
   },
--- config/plugins.ts
+++ config/plugins.ts
@@ -1,10 +1,20 @@
-export default {
-  // ...
+export default ({ env }) => ({
   upload: {
     config: {
+      provider: env('PROVIDER', 'local'),
       providerOptions: {
-        sizeLimit: 3000000000, // 1 GB
+        s3Options: {
+          accessKeyId: env('AWS_ACCESS_KEY_ID'),
+          secretAccessKey: env('AWS_ACCESS_SECRET'),
+          region: env('AWS_REGION'),
+          params: {
+            ACL: env('AWS_ACL', 'public-read'),
+            signedUrlExpires: env('AWS_SIGNED_URL_EXPIRES', 15 * 60),
+            Bucket: env('AWS_BUCKET'),
+          },
+        },
+        sizeLimit: 3000000000, // 3 GB
       },
     },
   },
-};
+});
